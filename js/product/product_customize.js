da4c3c2e4014d3e31e38f06e1cfa03183befbeb4
root <root@customiizer.com>
2025-06-14 04:59:57 +0200

    Revert "Merge pull request #45 from Alakaresh/o0173x-codex/corriger-taille-du-container-avec-image"
    This reverts commit cc3f1f803371bbe58ecf72a6ae42c31552ffe71e, reversing
    changes made to e0021085c89158d03341972de4495dce92e34512.

--- js/product/product_customize.js
+++ js/product/product_customize.js
@@ -212,7 +212,7 @@ jQuery(document).ready(function ($) {
                 const productPrice = jQuery(".price-value span").text().trim();
                 jQuery(".summary-price").text(productPrice);
                 imageControls.hide();
-                visualHeader.css('visibility', 'hidden');
+                visualHeader.hide();
                 trapFocus(customizeModal);
 
                 try {
@@ -258,7 +258,7 @@ jQuery(document).ready(function ($) {
                 releaseFocus(customizeModal);
                 addImageButton.show();
                 imageControls.hide();
-                visualHeader.css('visibility', 'hidden');
+                visualHeader.hide();
         });
 
         // Afficher le bouton lors du changement de produit
@@ -306,7 +306,7 @@ jQuery(document).ready(function ($) {
                 imageSourceModal.hide();
                 releaseFocus(imageSourceModal);
                 addImageButton.hide();
-                visualHeader.css('visibility', 'visible');
+                visualHeader.css('display', 'flex').show();
                 imageControls.css('display', 'flex').show();
         });
 
@@ -316,7 +316,7 @@ jQuery(document).ready(function ($) {
                 imageSourceModal.hide();
                 releaseFocus(imageSourceModal);
                 addImageButton.hide();
-                visualHeader.css('visibility', 'visible');
+                visualHeader.css('display', 'flex').show();
                 imageControls.css('display', 'flex').show();
         });
 
@@ -343,7 +343,7 @@ jQuery(document).ready(function ($) {
                 CanvasManager.removeImage();
                 addImageButton.show();
                 imageControls.hide();
-                visualHeader.css('visibility', 'hidden');
+                visualHeader.hide();
         });
 
         $(document).on('keydown', function (e) {
@@ -356,7 +356,6 @@ jQuery(document).ready(function ($) {
                                 releaseFocus(customizeModal);
                                 addImageButton.show();
                                 imageControls.hide();
-                                visualHeader.css('visibility', 'hidden');
                         }
                 }
 
@@ -364,7 +363,7 @@ jQuery(document).ready(function ($) {
                         CanvasManager.removeImage();
                         addImageButton.show();
                         imageControls.hide();
-                        visualHeader.css('visibility', 'hidden');
+                        visualHeader.hide();
                 }
         });
 
