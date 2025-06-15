85b1f1626a68f97366d27272b0a585c8f97aa1e6
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-06-15 02:36:52 +0200

    Add base64 image upload handler

---
+++ includes/save_base64_image.php
@@ -0,0 +1,26 @@
+<?php
+function save_image_from_base64() {
+    if (!isset($_POST['image_base64'])) {
+        wp_send_json_error(['message' => 'Paramètre image_base64 manquant.'], 400);
+    }
+
+    $data = $_POST['image_base64'];
+    $filename = isset($_POST['filename']) ? sanitize_file_name($_POST['filename']) : uniqid('img_') . '.png';
+
+    $data = preg_replace('#^data:image/\w+;base64,#i', '', $data);
+    $decoded = base64_decode($data);
+    if ($decoded === false) {
+        wp_send_json_error(['message' => 'Données base64 invalides.'], 400);
+    }
+
+    $upload_dir = wp_upload_dir();
+    $file_path = trailingslashit($upload_dir['path']) . $filename;
+    if (file_put_contents($file_path, $decoded) === false) {
+        wp_send_json_error(['message' => 'Erreur lors de la sauvegarde du fichier.'], 500);
+    }
+
+    $url = trailingslashit($upload_dir['url']) . $filename;
+    wp_send_json_success(['image_url' => $url]);
+}
+add_action('wp_ajax_save_image_from_base64', 'save_image_from_base64');
+add_action('wp_ajax_nopriv_save_image_from_base64', 'save_image_from_base64');
