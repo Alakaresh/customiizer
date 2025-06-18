c7502c840aa78d00cb15203f65a698535d7dd04f
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-06-18 07:46:30 +0200

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -4,6 +4,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function customiizer_api_permissions( WP_REST_Request $request ) {
+    error_log('Is user logged in? ' . (is_user_logged_in() ? 'YES' : 'NO'));
+    error_log('User capabilities: ' . print_r(wp_get_current_user()->allcaps, true));
+
     $token = $request->get_header( 'X-Customiizer-Token' );
 
     if ( $token && defined( 'CUSTOMIIZER_API_TOKEN' ) && hash_equals( CUSTOMIIZER_API_TOKEN, $token ) ) {
