4befe2c132a65807de8d18842f46a43ceb731f69
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-07-11 04:08:50 +0200

    Update use_points.js

--- js/loyalty/use_points.js
+++ js/loyalty/use_points.js
@@ -1,10 +1,10 @@
 jQuery(function($) {
     $(document).on('click', '#loyalty_points_button', function(e) {
         e.preventDefault();
-        var points = $(this).data('points') || 0;
-        $('#loyalty_points_to_use').val(999999); // On force l’utilisation max
 
-        // On the cart page submit the cart form so the discount is applied
+        // Injecte une valeur très haute, le PHP fera le tri
+        $('#loyalty_points_to_use').val(999999);
+
         var $cartForm = $('form.woocommerce-cart-form');
         if ($cartForm.length) {
             var $updateBtn = $cartForm.find('button[name="update_cart"]');
@@ -12,12 +12,10 @@ jQuery(function($) {
                 $updateBtn.prop('disabled', false);
                 $updateBtn.trigger('click');
             } else {
-                // Fallback: add hidden update parameter and submit
                 $cartForm.append('<input type="hidden" name="update_cart" value="1">');
                 $cartForm.trigger('submit');
             }
         } else {
-            // Checkout page - refresh totals
             $(document.body).trigger('update_checkout');
         }
     });
