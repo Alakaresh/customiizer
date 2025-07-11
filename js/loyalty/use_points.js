34282114627f292dfc468198e61292b94e5b1834
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-07-11 05:21:23 +0200

    Update use_points.js

--- js/loyalty/use_points.js
+++ js/loyalty/use_points.js
@@ -1,19 +1,14 @@
 jQuery(function($) {
     $(document).on('click', '#loyalty_points_button', function(e) {
         e.preventDefault();
-
-        // Injecte une valeur très haute, le PHP fera le tri
-        $('#loyalty_points_to_use').val(999999);
-
+        $('#loyalty_points_to_use').val(999999); // on force la valeur
         var $cartForm = $('form.woocommerce-cart-form');
         if ($cartForm.length) {
             var $updateBtn = $cartForm.find('button[name="update_cart"]');
             if ($updateBtn.length) {
-                $updateBtn.prop('disabled', false);
-                $updateBtn.trigger('click');
+                $updateBtn.prop('disabled', false).trigger('click');
             } else {
-                $cartForm.append('<input type="hidden" name="update_cart" value="1">');
-                $cartForm.trigger('submit');
+                $cartForm.append('<input type="hidden" name="update_cart" value="1">').trigger('submit');
             }
         } else {
             $(document.body).trigger('update_checkout');
