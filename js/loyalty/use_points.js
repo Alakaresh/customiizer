5cef1722bcb70dce9f5a42f13c5891d7f4820cb3
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-07-11 05:35:14 +0200

    Update use_points.js

--- js/loyalty/use_points.js
+++ js/loyalty/use_points.js
@@ -1,18 +1,17 @@
 jQuery(function($) {
     $(document).on('click', '#loyalty_points_button', function(e) {
         e.preventDefault();
-
-        const points = $(this).data('points') || 0;
+        var points = $(this).data('points') || 0;
         $('#loyalty_points_to_use').val(points);
 
-        console.log('[LOYALTY] Bouton cliqué, points =', points);
-
-        const $cartForm = $('form.woocommerce-cart-form');
+        // Forcer le recalcul du panier
+        var $cartForm = $('form.woocommerce-cart-form');
         if ($cartForm.length) {
-            $cartForm.find('button[name="update_cart"]').prop('disabled', false).trigger('click');
+            $cartForm.append('<input type="hidden" name="update_cart" value="1">');
+            $cartForm.trigger('submit');
         } else {
+            // Checkout page
             $(document.body).trigger('update_checkout');
         }
     });
 });
-
