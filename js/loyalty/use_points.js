01a92f1baa4f839a94f8c41e27cae9382ef8000a
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-07-11 05:22:58 +0200

    Update use_points.js

--- js/loyalty/use_points.js
+++ js/loyalty/use_points.js
@@ -1,17 +1,25 @@
 jQuery(function($) {
     $(document).on('click', '#loyalty_points_button', function(e) {
         e.preventDefault();
-        $('#loyalty_points_to_use').val(999999); // on force la valeur
-        var $cartForm = $('form.woocommerce-cart-form');
+
+        const points = $(this).data('points') || 0;
+        $('#loyalty_points_to_use').val(points);
+
+        console.log('[LOYALTY] Bouton cliqué, points =', points);
+
+        // Appel AJAX pour log côté PHP (optionnel mais utile)
+        $.post(customiizer_log_ajax.ajax_url, {
+            action: 'customiizer_log_loyalty_event',
+            context: 'JS',
+            message: 'Bouton cliqué avec ' + points + ' points'
+        });
+
+        const $cartForm = $('form.woocommerce-cart-form');
         if ($cartForm.length) {
-            var $updateBtn = $cartForm.find('button[name="update_cart"]');
-            if ($updateBtn.length) {
-                $updateBtn.prop('disabled', false).trigger('click');
-            } else {
-                $cartForm.append('<input type="hidden" name="update_cart" value="1">').trigger('submit');
-            }
+            $cartForm.find('button[name="update_cart"]').prop('disabled', false).trigger('click');
         } else {
             $(document.body).trigger('update_checkout');
         }
     });
 });
+
