ae0eba21ae5ad2faabe8d0ac3be58a12d1ffbde5
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-07-11 20:50:19 +0200

    Update loyalty widget with header and pages

--- js/loyalty/widget.js
+++ js/loyalty/widget.js
@@ -1,11 +1,27 @@
 jQuery(function($){
     const $button = $('<div id="loyalty-widget-button"><i class="fas fa-gift"></i></div>');
     const $popup = $('#loyalty-widget-popup');
+    const showPage = (slug) => {
+        $popup.find('.loyalty-widget-page').hide();
+        $popup.find('.loyalty-page-' + slug).show();
+    };
     $('body').append($button);
     $button.on('click', function(){
         $popup.toggleClass('open');
+        if($popup.hasClass('open')){
+            showPage('main');
+        }
     });
     $('#loyalty-widget-close').on('click', function(){
         $popup.removeClass('open');
+    });
+    $popup.find('.loyalty-how-get').on('click', function(){
+        showPage('get');
+    });
+    $popup.find('.loyalty-how-use').on('click', function(){
+        showPage('use');
+    });
+    $popup.find('.loyalty-back-main').on('click', function(){
+        showPage('main');
     });
 });
