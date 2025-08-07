5300c1f85ce58370c281abe239003063638092c4
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-08-07 19:56:54 +0200

    Update modal-cart.php

--- templates/modal-cart.php
+++ templates/modal-cart.php
@@ -3,11 +3,19 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 ?>
-<div id="cartModal" class="cart-modal" style="display:none;">
-    <div class="cart-modal-content">
-        <div class="close-cart">✖</div>
-        <?php if ( function_exists( 'woocommerce_mini_cart' ) ) {
-            woocommerce_mini_cart();
-        } ?>
+<div id="cartModal" class="cart-modal">
+  <div class="cart-modal-content">
+    <div class="cart-header">
+      <span class="cart-title">Mon panier</span>
+      <div class="close-cart">×</div>
     </div>
+    <div class="cart-body">
+      <?php if ( function_exists( 'woocommerce_mini_cart' ) ) {
+          woocommerce_mini_cart();
+      } ?>
+    </div>
+    <div class="cart-footer">
+      <a href="<?php echo wc_get_checkout_url(); ?>" class="checkout-button">Poursuivre la commande</a>
+    </div>
+  </div>
 </div>
