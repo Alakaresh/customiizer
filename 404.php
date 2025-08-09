dabe8e1e962f97326690f792c6da8b7a9b420a7e
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-08-09 21:17:44 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,14 +1,9 @@
-<?php
-get_header();
-?>
-
+<?php get_header(); ?>
 <main id="site-content" class="site-content">
-  <section class="error-404">
-    <h1>Page introuvable</h1>
+  <section class="error-404" role="region" aria-labelledby="t404">
+    <h1 id="t404">Page introuvable</h1>
     <p>La page que vous recherchez n'existe pas ou a été déplacée.</p>
-    <a href="<?php echo esc_url( home_url( '/home' ) ); ?>" class="lien">Retour à l'accueil</a>
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="lien">Retour à l'accueil</a>
   </section>
 </main>
-
 <?php get_footer(); ?>
-
