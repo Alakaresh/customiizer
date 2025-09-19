9e295ef7e0ba59fc5498927ca8aeb347cb2f979d
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-09-19 09:10:03 +0200

    Tidy user modal markup after restyle

--- templates/modal-user.php
+++ templates/modal-user.php
@@ -1,42 +1,37 @@
 <head>
-	<meta charset="utf-8">
+        <meta charset="utf-8">
        <!-- Style moved to assets.php -->
 </head>
 
 <div id="userModal" class="userModal" style="display:none;">
-	<div class="modal-content">
-		<!-- Section du titre avec uniquement le nom d'utilisateur -->
-		<div class="user-info">
-			<div class="user-name" style="text-align: center; background-color: #222; padding: 10px;">
-				<?php 
-				$current_user = wp_get_current_user();
-				echo esc_html($current_user->display_name); 
-				?>
-			</div>
-		</div>
+        <div class="modal-content">
+                <!-- Section du titre avec uniquement le nom d'utilisateur -->
+                <div class="user-info">
+                        <div class="user-name">
+                                <?php
+                                $current_user = wp_get_current_user();
+                                echo esc_html($current_user->display_name);
+                                ?>
+                        </div>
+                </div>
 
-		<!-- Conteneur pour la partie basse -->
-		<div class="lower-container">
-			<!-- Liens vers d'autres sections -->
-			<button class="modal-button" onclick="location.href='<?php echo home_url('/compte'); ?>'">
-				<i class="fas fa-id-badge"></i> Mon compte
-			</button>
-			<button class="modal-button" onclick="location.href='<?php echo home_url('/communaute'); ?>'">
-				<i class="fas fa-users"></i> Communauté
-			</button>
-			<a href="/compte?triggerClick=true" id="myCreationsLinkModal" class="modal-button" style="color: white;">
-				<i class="fas fa-images"></i> Mes créations
-			</a>
-
-
-
-
-
-                        <button class="modal-button" onclick="location.href='<?php echo wp_logout_url(home_url('/home')); ?>'">
+                <!-- Conteneur pour la partie basse -->
+                <div class="lower-container">
+                        <!-- Liens vers d'autres sections -->
+                        <button class="modal-button" type="button" onclick="location.href='<?php echo home_url('/compte'); ?>'">
+                                <i class="fas fa-id-badge"></i> Mon compte
+                        </button>
+                        <button class="modal-button" type="button" onclick="location.href='<?php echo home_url('/communaute'); ?>'">
+                                <i class="fas fa-users"></i> Communauté
+                        </button>
+                        <a href="/compte?triggerClick=true" id="myCreationsLinkModal" class="modal-button">
+                                <i class="fas fa-images"></i> Mes créations
+                        </a>
+                        <button class="modal-button modal-button--logout" type="button" onclick="location.href='<?php echo wp_logout_url(home_url('/home')); ?>'">
                                 <i class="fas fa-sign-out-alt"></i> Déconnexion
                         </button>
-		</div>
-	</div>
+                </div>
+        </div>
 </div>
 
 <!-- Script moved to assets.php -->
