10f591e4a4dda7f6e7b210e7945093332722a851
Alakaresh <32971375+Alakaresh@users.noreply.github.com>
2025-09-25 06:31:39 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,31 +9,6 @@
 if ( ! defined( 'ABSPATH' ) ) {
         exit; // Sortir si accédé directement.
 }
-add_action( 'wp_enqueue_scripts', function() {
-    // Ajout d'un script inline après le script Stripe de WooCommerce
-    wp_add_inline_script( 'wc-stripe', "
-        jQuery( function( $ ) {
-            if ( typeof wc_stripe_form !== 'undefined' ) {
-                var darkStyle = {
-                    base: {
-                        color: '#f6f9f9',                        // texte
-                        fontSize: '16px',
-                        fontFamily: 'Inter, sans-serif',
-                        iconColor: '#1abc9c',                    // icônes Visa/Mastercard
-                        '::placeholder': { color: 'rgba(230,230,230,0.55)' }
-                    },
-                    invalid: {
-                        color: '#ff6b6b',                        // erreurs
-                        iconColor: '#ff6b6b'
-                    }
-                };
-                // Injection du style sombre
-                wc_stripe_form.elements_options = { style: darkStyle };
-            }
-        });
-    ");
-});
-
 
 add_filter(
         'body_class',
